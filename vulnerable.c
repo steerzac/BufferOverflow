96f2a8e3b0b95a9dbc67f77eafada30d5c82cb36
steerzac <steerzac@gmail.com>
2017-07-22 12:43:14 -0700

    Buffer overflow attack successful with input.txt piped to ./a.out

--- vulnerable.c
+++ vulnerable.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
-void foo(char* input) {
-  char buf[10];
+void foo() {
+  char buf[20];
+  printf("Enter some text:\n");
+  scanf("%s", &buf);
+  printf("You entered: %s\n", buf);
 
-  printf("Stack looks like");
-  printf("\n %p\n %p\n %p\n %p\n %p\n %p\n \n");
-
-  strcpy(buf, input);
-  printf("%s\n", buf);
-
-  printf("\n %p\n %p\n %p\n %p\n %p\n %p\n \n");
 }
 
 void malicious() {
-  printf("Muahaha. I hacked the computer.");
+  printf("Muahaha. I hacked the computer.\n");
 }
 
 int main(int argc, char* argv[]) {
@@ -22,11 +18,6 @@ int main(int argc, char* argv[]) {
   printf("Address of foo: %p\n", foo);
   printf("Address of main: %p\n", main);
 
-  if(argc != 2) {
-    printf("Usage: ./a.out <maliciousString>");
-    return -1;
-  }
-
-  foo(argv[1]);
+  foo();
   return 0;
 }
